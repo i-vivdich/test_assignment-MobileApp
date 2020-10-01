fcca6e9141930123d97d7d0e734f76c347007917
ryslanuch <ryslanartamonov786@gmail.com>
2020-10-01 21:45:25 +0300

    Test Scene commit

---
+++ ChemicsApp/src/scenes/main/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, View, Text, Button, Alert } from 'react-native';
+
+const buttonHandler = (source) => {
+    return source === 'register' ? Alert.alert('register') : Alert.alert('login');
+}
+
+const MainScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Dry Cleaner's: All</Text>
+            <Button
+            title="Register"
+            color="lime"
+            onPress={buttonHandler.bind(this, 'register')}    
+            />
+            </View>
+        </SafeAreaView>
+    );
+    }
+    
+    export default MainScreen;
